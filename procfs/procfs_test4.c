2e2b0e6f8f5c7f21fd967e2bc5edbb79fd91d50a
Fu Yajun <fuyajun1983cn@163.com>
2017-01-13 23:41:05 +0800

    add proc_create_data使用示例

--- procfs/procfs_test4.c
+++ procfs/procfs_test4.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c)  2016     Yajun Fu (fuyajun1983cn@163.com)
+ * Copyright (c)  2017     Yajun Fu (fuyajun1983cn@163.com)
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,6 +14,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+/**
+ *  proc_create_data(...)接口使用实例
+ */
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -21,80 +25,49 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
+#include <linux/sched.h> //for current
+#include <linux/vmalloc.h>
 
-/**
- *     Seq file test
- */
+/* dummy test structure */
+struct priv_data {
+  int data;
+};
+
 //Entries for /proc/gdl and /proc/gdl/memory
 static struct proc_dir_entry *mm_proc_mem;
 static struct proc_dir_entry *mm_proc_dir;
 
-//only to how at most 10 characters
-#define MAX_COUNT  10
+//only output some info from kernel
 
-unsigned char strings[80];
-
-static ssize_t procfs_test2_write(struct file * file, 
-                                  const char  __user * buffer, 
-                                  size_t count, 
-                                  loff_t *        data)
+static int procfs_test4_show(struct seq_file *m, void *v)
 {
-  size_t   size_to_copy;
-  size_to_copy = count;
-  memset(strings, 0x0, 80);
-  copy_from_user(strings, buffer, size_to_copy);
-  printk("%s", strings);
-  return size_to_copy;
-}
-
-static void  *procfs_test2_seq_start(struct seq_file *f, loff_t *pos)
-{
-  return (*pos < MAX_COUNT) ? pos : NULL;
-}
-
-static void  *procfs_test2_seq_next(struct seq_file *f, void *v, loff_t *pos)
-{
-  printk("current pos: %d\n", (int)(*pos));
-  (*pos)++;
-  if (*pos >= MAX_COUNT)
-    return NULL;
-  return pos;
-}
-
-static void  procfs_test2_seq_stop(struct seq_file *f, void *v)
-{
-  /* Nothing to do */
-}
-
-static int  procfs_test2_show(struct seq_file *pi, void *v)
-{
-  unsigned int i = *(loff_t *) v;
-  seq_putc(pi, strings[i]);
+  struct priv_data *data = (struct priv_data*)m->private;
+  if (data != NULL) {
+    seq_printf(m, "Get Private Data: %d\n", data->data);
+    vfree(data);
+  } else
+    seq_printf(m, "NO Private Data\n");
   return 0;
 }
 
-static const struct seq_operations procfs_test2_seq_ops = {
-  .start = procfs_test2_seq_start,
-  .next  = procfs_test2_seq_next,
-  .stop  = procfs_test2_seq_stop,
-  .show  = procfs_test2_show
-};
-
-static int  procfs_test2_open(struct inode *inode, struct file *filp)
+static int procfs_test4_open(struct inode *inode, struct file *file)
 {
-  return seq_open(filp, &procfs_test2_seq_ops);
+  
+  return single_open(file, procfs_test4_show, PDE_DATA(inode));
 }
 
-static const struct file_operations procfs_test2_fops = {
-  .open           = procfs_test2_open,
+static const struct file_operations procfs_test4_fops = {
+  .open           = procfs_test4_open,
   .read           = seq_read,
   .llseek         = seq_lseek,
   .release        = seq_release,
-  .write         = procfs_test2_write,
 };
 
-static int __init procfs_test2_init(void)
+static int __init procfs_test4_init(void)
 {
+  struct priv_data *data = (struct priv_data*)vmalloc(sizeof(*data));
+  memset(data, 0, sizeof(struct priv_data));
+  data->data = 1;
   mm_proc_dir = 0;
   mm_proc_mem = 0;
 
@@ -106,7 +79,7 @@ static int __init procfs_test2_init(void)
   }
 
   //create /proc/gdl/memory file
-  mm_proc_mem = proc_create("memory", S_IFREG|S_IRWXU|S_IRWXG|S_IRWXO, mm_proc_dir, &procfs_test2_fops);
+  mm_proc_mem = proc_create_data("memory", S_IFREG|S_IRWXU|S_IRWXG|S_IRWXO, mm_proc_dir, &procfs_test4_fops, data);
   if (mm_proc_mem == 0) {
     printk(KERN_ERR "/proc/gdl/memory creation failed\n");
     proc_remove(mm_proc_dir);
@@ -117,7 +90,7 @@ static int __init procfs_test2_init(void)
   return 0;
 }
 
-static void __exit procfs_test2_exit(void)
+static void __exit procfs_test4_exit(void)
 {
   if (mm_proc_dir != 0)
     {
@@ -131,7 +104,8 @@ static void __exit procfs_test2_exit(void)
       mm_proc_dir = 0;
     }
 }
-module_init(procfs_test2_init);
-module_exit(procfs_test2_exit);
+module_init(procfs_test4_init);
+module_exit(procfs_test4_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yajun Fu<fuyajun1983cn@163.com");
+
